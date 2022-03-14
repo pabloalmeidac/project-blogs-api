35c70c088679bf1078291bba8159f3f4eff03e9d
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-14 18:34:30 -0300

    feat: router user/me

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const validateUser = require('../middlewares/validateUser');
 const auth = require('../middlewares/auth');
-const { create, getAll, getById } = require('../controllers/userController');
+const { create, getAll, getById, exclude } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/', validateUser, create);
 router.get('/', auth, getAll);
 router.get('/:id', auth, getById);
+router.delete('/me', auth, exclude);
 
 module.exports = router;
