08c082439afa42fa3d0e0d7a1046f99ece4f67d1
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-14 15:45:55 -0300

    feat: add router get: user by id

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const validateUser = require('../middlewares/validateUser');
 const auth = require('../middlewares/auth');
-const { create, getAll } = require('../controllers/userController');
+const { create, getAll, getById } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/', validateUser, create);
 router.get('/', auth, getAll);
+router.get('/:id', auth, getById);
 
 module.exports = router;
