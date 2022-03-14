34c6a1b3e3b7f6bab50f3b1e89209a8909d5aff1
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-14 19:15:29 -0300

    feat: add route list all categories

--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const validateCategory = require('../middlewares/validateCategory');
-const { create } = require('../controllers/categoryController');
+const { create, getAll } = require('../controllers/categoryController');
 const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/', validateCategory, auth, create);
+router.get('/', auth, getAll);
 
 module.exports = router;
