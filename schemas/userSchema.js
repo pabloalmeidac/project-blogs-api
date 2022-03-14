7cf5a080fd035a983fdb693bc97fbc26473285a4
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-14 11:34:22 -0300

    feat: create schemas for validation user

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -3,14 +3,14 @@ const Joi = require('joi');
 const userSchema = Joi.object({
   displayName: Joi.string().min(8).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().length(6).required(),
   image: Joi.string().required(),
 }).messages({
   'any.required': '400|{{ #label }} is required',
-  'string.base': '422|{{ #label }} must be a string',
-  'string.email': '422|{{ #label }} must be a string',
-  'string.min': '422|{{ #label }} length must be at least {{ #limit }} characters long',
-  'number.positive': '422|{{ #label }} must be greater than or equal to 1',
+  'string.base': '400|{{ #label }} must be a string',
+  'string.email': '400|{{ #label }} must be a valid email',
+  'string.min': '400|{{ #label }} length must be at least {{ #limit }} characters long',
+  'string.length': '400|{{ #label }} length must be {{ #limit }} characters long',
 });
 
 const validate = (objUser) => userSchema.validate(objUser);
