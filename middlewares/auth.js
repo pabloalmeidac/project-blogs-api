8ed238e48602ca6bef537e4cfb5e48570236625a
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-14 15:32:50 -0300

    feat: fix auth jwt

---
+++ middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.payload;
+
+    next();
+  } catch (error) {
+    if (error.name.includes('Token')) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+    next(error);
+  }
+};
