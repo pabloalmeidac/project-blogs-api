fe04bc89755e5ace21aad6679df74b314523db4f
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-03-18 15:29:43 -0300

    Refactor ValidateLogin

--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -1,12 +1,5 @@
 const loginSchema = require('../schemas/loginSchema');
 
-/* const verifyEmpty = (email, password) => {
-  const obj = { code: 400, label: '' };
-  obj.label = !email ? 'email' : 'password';
-
-  if (!email || !password) return obj;
-}; */
-
 module.exports = (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -18,13 +11,6 @@ module.exports = (req, res, next) => {
       return res.status(code).json({ message });
     }
     
-    /* const errorEmpty = verifyEmpty(email, password);
-
-    if (errorEmpty) {
-      return res.status(errorEmpty.code).json({ 
-        message: `"${errorEmpty.label}" is not allowed to be empty`,
-      });
-    } */
     return next();
   } catch (error) {
     next(error);
