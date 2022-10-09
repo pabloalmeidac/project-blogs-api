28fc5e062722e9440d707bfc79ac1aa002ba696d
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-10-09 11:49:42 -0300

    refactor: remove unnecessary

--- index.js
+++ index.js
@@ -15,8 +15,3 @@ app.use('/categories', categoryRouter);
 app.use('/post', postRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
